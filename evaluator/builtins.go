85d65c6251f0311079b7076961439ec368eaec60
Sebastiaan <sebastiaan.wouters@student.kuleuven.be>
2023-03-07 12:51:53 +0100

    add support for sqrt, pow, isPrime

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -2,6 +2,8 @@ package evaluator
 
 import (
 	"fmt"
+	"math"
+	"math/big"
 
 	"github.com/SebastiaanWouters/verigo/object"
 )
@@ -20,6 +22,60 @@ var builtins = map[string]*object.Builtin{
 				return newError("argument to `len` not supported, got %s",
 					args[0].Type())
 			}
+		},
+	},
+	"pow": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 2 {
+				return newError("wrong number of arguments. got=%d, want=1",
+					len(args))
+			}
+			switch arg1 := args[0].(type) {
+			case *object.Integer:
+				switch arg2 := args[1].(type) {
+				case *object.Integer:
+					return &object.Integer{Value: int64(math.Pow(float64(arg1.Value), float64(arg2.Value)))}
+				default:
+					return newError("argument to `pow` not supported, got %s",
+						arg2.Type())
+				}
+			default:
+				return newError("argument to `pow` not supported, got %s",
+					arg1.Type())
+			}
+
+		},
+	},
+	"sqrt": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1",
+					len(args))
+			}
+			switch arg := args[0].(type) {
+			case *object.Integer:
+				return &object.Integer{Value: int64(math.Sqrt(float64(arg.Value)))}
+			default:
+				return newError("argument to `pow` not supported, got %s",
+					arg.Type())
+			}
+
+		},
+	},
+	"isPrime": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1",
+					len(args))
+			}
+			switch arg := args[0].(type) {
+			case *object.Integer:
+				return &object.Boolean{Value: big.NewInt(arg.Value).ProbablyPrime(0)}
+			default:
+				return newError("argument to `pow` not supported, got %s",
+					arg.Type())
+			}
+
 		},
 	},
 	"print": &object.Builtin{
